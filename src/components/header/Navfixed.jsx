05b87586efda7dba705b1197cedf26b9272bd18a
Manuchehr <manuchehrsaidov998@gmail.com>
2023-08-21 10:49:03 +0500

    true-false-navbar-commit

--- src/components/header/Navfixed.jsx
+++ src/components/header/Navfixed.jsx
@@ -12,7 +12,7 @@ import style from "./navfixed.module.css";
 
 // eslint-disable-next-line react/prop-types
 const Navfixed = ({ sticky }) => {
-  const initialCloseValue = JSON.parse(localStorage.getItem("close")) || true;
+  const initialCloseValue = JSON.parse(localStorage.getItem("close")) || false;
   const [close, setClose] = useState(initialCloseValue);
 
   // Установить значение close в localStorage при изменении close
